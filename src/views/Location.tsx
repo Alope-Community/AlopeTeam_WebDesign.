10dc1bbb3b984331b341baf1c30739849bf143b4
Saeful Rohman <Saefulgaming72@gmail.com>
2024-10-06 20:14:02 +0700

    GYTM-13:Section About

--- src/views/Location.tsx
+++ src/views/Location.tsx
@@ -1,9 +1,11 @@
+import LocationAboutSection from "../section/Location/About";
 import LocationAccordionSection from "../section/Location/Accordion";
 
 export default function LocationView() {
   return (
     <>
       <LocationAccordionSection />
+      <LocationAboutSection />
     </>
   );
 }
