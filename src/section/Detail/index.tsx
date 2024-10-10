04821684d6597d83fa7b42b29b00b86173b17998
taufan12345 <abaysurbakti17@gmail.com>
2024-10-10 11:36:59 +0700

    GYTM-54 FIX WORDING ALL PAGE

--- src/section/Detail/index.tsx
+++ src/section/Detail/index.tsx
@@ -3,31 +3,23 @@ export default function DetailMenuSection() {
     <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 pt-10 lg:pt-28 gap-8 lg:gap-16 px-6 lg:px-20">
       <div className="col-span-2 text-gray-100">
         <p className="text-[13px] md:text-base mb-6 md:mb-10 leading-6 md:leading-8">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          dolore laborum eius ratione blanditiis sed vero ducimus nostrum
-          corrupti cupiditate quaerat, possimus voluptatum non sint veniam ex
-          dolores suscipit commodi Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Voluptas placeat ex rerum mollitia enim natus ab
-          adipisci laboriosam labore nulla praesentium, nostrum, similique saepe
-          dicta a quaerat ad necessitatibus unde?
+          Sebagai CEO dari Prime Chicken, saya dengan bangga menyatakan bahwa
+          cabang kami di Surabaya telah mencapai standar tertinggi dalam hal
+          kualitas makanan dan layanan. Kami selalu memastikan setiap hidangan
+          yang kami sajikan dipersiapkan dengan bahan-bahan terbaik dan resep
+          autentik yang telah diwariskan selama bertahun-tahun. Tidak hanya dari
+          kami, hasil testimoni pelanggan pun menyebutkan bahwa cita rasa ayam
+          di cabang ini tak tertandingi, dengan banyak yang memuji kelezatan
+          serta kerenyahan ayam kami.
         </p>
         <p className="text-[13px] md:text-base mb-6 md:mb-10 leading-6 md:leading-8">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          dolore laborum eius ratione blanditiis sed vero ducimus nostrum
-          corrupti cupiditate quaerat, possimus voluptatum non sint veniam ex
-          dolores suscipit commodi Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Voluptas placeat ex rerum mollitia enim natus ab
-          adipisci laboriosam labore nulla praesentium, nostrum, similique saepe
-          dicta a quaerat ad necessitatibus unde?
-        </p>
-        <p className="text-[13px] md:text-base mb-6 md:mb-10 leading-6 md:leading-8">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-          dolore laborum eius ratione blanditiis sed vero ducimus nostrum
-          corrupti cupiditate quaerat, possimus voluptatum non sint veniam ex
-          dolores suscipit commodi Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Voluptas placeat ex rerum mollitia enim natus ab
-          adipisci laboriosam labore nulla praesentium, nostrum, similique saepe
-          dicta a quaerat ad necessitatibus unde?
+          Saya yakin, cabang Prime Chicken di Surabaya menjadi yang terbaik
+          karena komitmen kami yang kuat terhadap kepuasan pelanggan. Testimoni
+          dari pelanggan kami juga membuktikan hal tersebut, dengan banyak yang
+          menyatakan bahwa mereka selalu kembali karena kualitas makanan yang
+          konsisten dan pelayanan yang luar biasa. Kepercayaan yang diberikan
+          pelanggan adalah motivasi terbesar kami untuk terus menjadi yang
+          terdepan dalam industri ini.
         </p>
       </div>
       <div>
@@ -41,24 +33,16 @@ export default function DetailMenuSection() {
             Taufan Hidayatul Akbar
           </h4>
           <p className="text-gray-100 text-sm mt-4 text-center md:text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-            debitis voluptatibus ut laboriosam quibusdam tempore.
+            Sebagai CEO, Founder Cluck n Roll, sekaligus Manager Cabang
+            Surabaya, saya berkomitmen menyajikan hidangan ayam berkualitas
+            tinggi dan pengalaman bersantap terbaik bagi pelanggan.
           </p>
           <div className="mt-6 md:mt-12 text-center md:text-left">
             <span className="bg-red-500/20 text-red-500 font-semibold px-2 py-1 rounded text-[12px] mx-1 mb-2 inline-block">
-              ingridient
+              CEO And Founder
             </span>
             <span className="bg-yellow-500/20 text-yellow-500 font-semibold px-2 py-1 rounded text-[12px] mx-1 mb-2 inline-block">
-              ingridient
-            </span>
-            <span className="bg-emerald-500/20 text-emerald-500 font-semibold px-2 py-1 rounded text-[12px] mx-1 mb-2 inline-block">
-              ingridient
-            </span>
-            <span className="bg-purple-500/20 text-purple-500 font-semibold px-2 py-1 rounded text-[12px] mx-1 mb-2 inline-block">
-              ingridient
-            </span>
-            <span className="bg-blue-500/20 text-blue-500 font-semibold px-2 py-1 rounded text-[12px] mx-1 mb-2 inline-block">
-              ingridient
+              Manager Surabaya
             </span>
           </div>
         </div>
